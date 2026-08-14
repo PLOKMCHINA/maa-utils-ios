d06775808b349b4b07aa7df55de19685741100ce
PLOKMCHINA <PLOKMCHINA@users.noreply.github.com>
2026-08-15 05:04:51 +0800

    IAA: iOS 裁剪 boost::process（BoostIO.hpp + MAA_UTILS_IOS 宏）

--- include/MaaUtils/IOStream/BoostIO.hpp
+++ include/MaaUtils/IOStream/BoostIO.hpp
@@ -3,7 +3,11 @@
 #define BOOST_PROCESS_USE_STD_FS 1
 
 #include <boost/asio.hpp>
+// IAA: iOS 沙盒无子进程支持，boost::process（依赖 wordexp）不可用，
+// 且 ChildPipeIOStream（唯一使用者）已在 iOS 构建中排除
+#ifndef MAA_UTILS_IOS
 #include <boost/process.hpp>
+#endif
 #ifdef _WIN32
 #include <boost/process/extend.hpp>
 #include <boost/process/windows.hpp>
